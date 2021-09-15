51c89538250e1ae6b00b515663fff8f57d06739d
chris <letter2electricboy@hotmail.com>
2021-09-16 01:13:38 +0800

    edit run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#from selenium import webdriver
+from selenium import webdriver
 import requests
 
 headers = {
@@ -8,22 +8,22 @@ headers = {
 
 
 if __name__ == '__main__':
-    #chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #chrome_options.add_argument('--no-sandbox')
-    #chrome_options.add_argument('--headless')
-    #chrome_options.add_argument('--disable-gpu')
-    #driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    # maximize with maximize_window()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    #maximize with maximize_window()
     #driver.maximize_window()
-    #driver.get("https://www.tutorialspoint.com/index.htm")
-    # identify element
-    #l=driver.find_element_by_css_selector("h4")
+    driver.get("https://www.tutorialspoint.com/index.htm")
+     identify element
+    l=driver.find_element_by_css_selector("h4")
     # get text and print
     #print("Text is: " + l.text)
 
-    params = {"message": "!!test from selenium!!!"}
+    params = {"message": f"{l.text}"}
     r = requests.post("https://notify-api.line.me/api/notify",
                       headers=headers, params=params)
 
-    #driver.close()
+    driver.close()
